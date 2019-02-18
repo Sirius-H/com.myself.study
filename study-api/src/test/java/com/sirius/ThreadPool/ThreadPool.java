698042c151f68ff80d48f896e0591d30e372d939
Sirius <981800017@qq.com>
2019-02-18 23:04:27 +0800

    线程池技术

---
+++ study-api/src/test/java/com/sirius/ThreadPool/ThreadPool.java
@@ -0,0 +1,15 @@
+package com.sirius.ThreadPool;
+
+public interface ThreadPool<Job extends Runnable> {
+
+    //执行job
+    void execute (Job job);
+    //关闭线程池
+    void shutdown();
+    // 增加工作线程
+    void addWorkers(int num);
+    //减少工作线程
+    void removeWorkers(int num);
+    //获取正在等待执行的任务数
+    int getJobSize();
+}
