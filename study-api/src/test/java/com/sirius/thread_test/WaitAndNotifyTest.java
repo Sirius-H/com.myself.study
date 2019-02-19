d8cdca6a9c1dbd278816bfd4b6474069aa995c74
Sirius <981800017@qq.com>
2019-02-19 09:59:03 +0800

    临时提交

--- study-api/src/test/java/com/sirius/thread_test/WaitAndNotifyTest.java
+++ study-api/src/test/java/com/sirius/thread_test/WaitAndNotifyTest.java
@@ -1,6 +1,7 @@
 package com.sirius.thread_test;
 
 import com.sirius.BaseTest;
+import org.testng.annotations.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -22,6 +23,57 @@ public class WaitAndNotifyTest extends BaseTest {
 
     }
 
+
+    /**
+     * 测试
+     */
+    @Test
+    public void test_Join() {
+        System.out.println("-------主方法执行-------");
+
+        Thread joinTest = new Thread( new JoinTest(Thread.currentThread()), "join" );
+        joinTest.start();
+        sleepUtil(2000);
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             Thread joinTest2 = new Thread( new JoinTest2(), "joinTest2" );
+        joinTest2.start();
+        System.out.println("joinTest2 启动了");
+
+        System.out.println("运行结束了");
+    }
+
+
+
+    static class JoinTest implements Runnable {
+
+        private Thread previous;
+
+        public JoinTest(Thread previous) {
+            this.previous = previous;
+        }
+
+        public void run() {
+            synchronized (lock) {
+                System.out.println("----------"+Thread.currentThread().getName()+"+执行完毕----------");
+                try {
+                    previous.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    static class JoinTest2 implements Runnable {
+
+        public void run() {
+            synchronized (lock) {
+                System.out.println("----------"+Thread.currentThread().getName()+"抢到了锁----------");
+                sleepUtil(5000);
+            }
+        }
+    }
 
 
     static class Wait implements Runnable {
