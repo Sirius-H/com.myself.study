957b507545efc7c49eb9945ad6be14568596d1a1
Sirius <981800017@qq.com>
2019-02-26 22:47:40 +0800

    simple http server

---
+++ study-api/src/test/java/com/sirius/ThreadPool/simple_http_server/SimpleHttpServer.java
@@ -0,0 +1,88 @@
+package com.sirius.ThreadPool.simple_http_server;
+
+import com.sirius.ThreadPool.DefaultThreadPool;
+import com.sirius.ThreadPool.ThreadPool;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleHttpServer {
+
+    static ThreadPool<HttpRequestHandler> threadPool = new DefaultThreadPool<>();
+
+    static String basePath;
+
+    static ServerSocket serverSocket;
+
+    static int port = 8080;
+
+
+    public static void setPort(int port) {
+        if (port > 0) {
+            SimpleHttpServer.port = port;
+        }
+    }
+
+
+    public static void setBasePath (String basePath){
+        SimpleHttpServer.basePath = basePath;
+    }
+
+    public static void start () throws Exception{
+        serverSocket = new ServerSocket(port);
+        Socket socket = null;
+        while ((socket = serverSocket.accept()) != null){
+            threadPool.execute(new HttpRequestHandler(socket));
+        }
+        serverSocket.close();
+    }
+
+    static class HttpRequestHandler implements Runnable{
+
+        private Socket socket;
+
+
+        public HttpRequestHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            String line;
+            BufferedReader br = null;
+            BufferedReader reader = null;
+            PrintWriter out = null;
+            InputStream in = null;
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String header = reader.readLine();
+                String filePath = basePath + header.split(" ")[1];
+                out = new PrintWriter(socket.getOutputStream());
+
+                if (filePath.endsWith("jpg") || filePath.endsWith("ico")) {
+                    in = new FileInputStream(filePath);
+
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    int i = 0;
+                    while ((i = in.read()) != -1) {
+                        baos.write(i);
+                    }
+                    byte[] bytes = baos.toByteArray();
+                    out.println("HTTP/1.1 200 OK");
+                    out.println("Server: Molly");
+                    out.println("Content-Type: image/jpeg");
+                    out.println("Content-Length:" + bytes.length);
+                    out.println("");
+                    socket.getOutputStream().write(bytes,0,bytes.length);
+                }else {
+
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
