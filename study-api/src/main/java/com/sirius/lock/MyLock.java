b6301d942478c32c135b7569df212e67dd508282
Sirius <981800017@qq.com>
2019-03-18 21:59:24 +0800

    临时提交

--- study-api/src/main/java/com/sirius/lock/MyLock.java
+++ study-api/src/main/java/com/sirius/lock/MyLock.java
@@ -1,6 +1,9 @@
 package com.sirius.lock;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
 
 /**
  * 描述:
@@ -8,31 +11,82 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
  * @author tangzhiming
  * @create 2019-03-05 13:16
  */
-public class MyLock extends AbstractQueuedSynchronizer {
+public class MyLock implements Lock {
 
+    private Sync sync = new Sync();
 
     @Override
-    protected boolean tryAcquire(int arg) {
-        return super.tryAcquire(arg);
+    public void lock() {
+        sync.acquire(1);
     }
 
     @Override
-    protected boolean tryRelease(int arg) {
-        return super.tryRelease(arg);
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
     }
 
     @Override
-    protected int tryAcquireShared(int arg) {
-        return super.tryAcquireShared(arg);
+    public boolean tryLock() {
+        return sync.tryAcquire(1);
     }
 
     @Override
-    protected boolean tryReleaseShared(int arg) {
-        return super.tryReleaseShared(arg);
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return sync.tryAcquireNanos(1,unit.toNanos(time));
     }
 
     @Override
-    protected boolean isHeldExclusively() {
-        return super.isHeldExclusively();
+    public void unlock() {
+        sync.tryRelease(0);
     }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+
+    private static class Sync extends AbstractQueuedSynchronizer {
+        /**
+         * 独占式 获取锁
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryAcquire(int arg) {
+            if (compareAndSetState(0,arg)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        /**
+         *独占式释放锁
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryRelease(int arg) {
+            if (getState() == 0) {
+                throw new IllegalArgumentException();
+            }
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        /**
+         * 用来表示同步器是否在独占模式下被线程占用
+         * @return
+         */
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
 }
---
+++ study-api/src/test/java/com/sirius/lock/LockApiTest.java
@@ -0,0 +1,14 @@
+package com.sirius.lock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockApiTest {
+
+
+    static final Lock lock = new ReentrantLock();
+
+    public void lockTest () {
+
+    }
+}
